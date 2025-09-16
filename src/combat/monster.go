4b10ed416c0a4b0d3eea95958672494e49814819
Sarah <sarah.bouhadra@ynov.com>
2025-09-16 09:42:49 +0200

    2.1.1

--- src/combat/monster.go
+++ src/combat/monster.go
@@ -1,1 +1,18 @@
 package combat
+
+type Monster struct {
+	Name      string
+	MaxHP     int
+	CurrentHP int
+	Attack    int
+}
+
+func InitGoblin() Monster {
+	return Monster{
+		Name:      "Gobelin d'entraînement",
+		MaxHP:     40,
+		CurrentHP: 40,
+		Attack:    5,
+	}
+
+}
